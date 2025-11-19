66d0234dc8f27bf971f55cff1c91a97897a87405
Ayrik <AyrikHirad@gmail.com>
2025-11-19 13:34:27 +0330

    feat: update language and timezone settings to Persian and Tehran respectively; enhance token response messages in views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -73,7 +73,7 @@ class CookieTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
         refresh = request.COOKIES.get(settings.AUTH_COOKIE_REFRESH)
         if refresh is None:
-            return Response({'detail': 'توکن رفرش یافت نشد.'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'detail': 'توکن رفرش در کوکی یافت نشد.'}, status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = self.get_serializer(data={'refresh': refresh})
         serializer.is_valid(raise_exception=True)
@@ -81,7 +81,7 @@ class CookieTokenRefreshView(TokenRefreshView):
         new_refresh = serializer.validated_data.get('refresh')
         refresh_to_set = str(new_refresh) if new_refresh else refresh
 
-        response = Response({'detail': 'توکن جدید صادر شد.'}, status=status.HTTP_200_OK)
+        response = Response({'detail': 'توکن با موفقیت به روزرسانی شد.'}, status=status.HTTP_200_OK)
         _attach_tokens(response, access, refresh_to_set)
         return response
 
@@ -90,7 +90,7 @@ class LogoutView(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
-        response = Response({'detail': 'با موفقیت خارج شدید.'}, status=status.HTTP_200_OK)
+        response = Response({'detail': 'خروج با موفقیت انجام شد.'}, status=status.HTTP_200_OK)
         response.delete_cookie(settings.AUTH_COOKIE_ACCESS, path=settings.AUTH_COOKIE_PATH, domain=settings.AUTH_COOKIE_DOMAIN)
         response.delete_cookie(settings.AUTH_COOKIE_REFRESH, path=settings.AUTH_COOKIE_PATH, domain=settings.AUTH_COOKIE_DOMAIN)
         return response
